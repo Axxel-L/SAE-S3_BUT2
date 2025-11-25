20012514efd9f52dd49f53e04c2bba579c41f69c
SIMON Abdel-Malek <e45402u@etu.univ-lorraine.fr>
2025-11-25 15:28:39 +0100

    Séparation de index.htmlen footer header et index php

---
+++ app/footer.php
@@ -0,0 +1,46 @@
+
+    <!-- Footer -->
+    <footer class="glass-effect-footer py-16 px-6 mt-20 rounded-t-6xl modern-border">
+        <div class="container mx-auto">
+            <div class="flex flex-col md:flex-row justify-between items-center">
+                <div class="flex items-center space-x-4 mb-8 md:mb-0">
+                    <div class="glass-button p-2 rounded-3xl">
+                        <img src="./assets/img/logo.png" alt="Logo GameCrown" class="logo-image">
+                    </div>
+                    <span class="logo-text">
+                        GAME<span class="accent-gradient">CROWN</span>
+                    </span>
+                </div>
+
+                <div class="flex space-x-5">
+                    <a href="#"
+                        class="glass-button rounded-3xl p-3 w-12 h-12 flex items-center justify-center modern-border">
+                        <i class="fab fa-twitter text-accent"></i>
+                    </a>
+                    <a href="#"
+                        class="glass-button rounded-3xl p-3 w-12 h-12 flex items-center justify-center modern-border">
+                        <i class="fab fa-facebook-f text-accent"></i>
+                    </a>
+                    <a href="#"
+                        class="glass-button rounded-3xl p-3 w-12 h-12 flex items-center justify-center modern-border">
+                        <i class="fab fa-instagram text-accent"></i>
+                    </a>
+                    <a href="#"
+                        class="glass-button rounded-3xl p-3 w-12 h-12 flex items-center justify-center modern-border">
+                        <i class="fab fa-youtube text-accent"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="separator mt-10"></div>
+
+            <div class="text-center text-base text-light/70">
+                <p>&copy;2025 GameCrown. Tous droits réservés.</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="../assets/js/index.js"></script>
+</body>
+
+</html>
